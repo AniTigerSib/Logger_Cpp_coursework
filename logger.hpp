ae8da140298d6453b7f3062ecb152cbc81ab20a3
Michael Siberian <khudoleev04@mail.ru>
2023-12-06 03:30:07 +0500

    0.1.0.2

---
+++ logger.hpp
@@ -0,0 +1,14 @@
+#ifndef _LOGGER_HPP_
+#define _LOGGER_HPP_
+#include <iostream>
+#include <string>
+
+enum loglevel_e
+{
+    logEXTRA, logALARM, logCRITICAL, logERROR, logWARNING, logNOTIF, logINFO, logDEBUG
+};
+class Logger
+{
+
+};
+#endif
